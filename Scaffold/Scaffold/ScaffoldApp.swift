4af9f1819a3fd734eeb55cd5cc1227de3ff92cca
Logan Proffitt <lopr6007@colorado.edu>
2025-03-29 17:21:06 -0600

    Initial commit

---
+++ Scaffold/Scaffold/ScaffoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaffoldApp.swift
+//  Scaffold
+//
+//  Created by Logan Proffitt on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ScaffoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
